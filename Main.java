53bd3204cddf7c129cd28891250bf50cce015f6b
Cypi <cyprien.mangin@m4x.org>
2012-05-19 17:50:59 +0200

    Modified Main for new tests
    Signed-off-by: Cypi <cyprien.mangin@m4x.org>

--- Main.java
+++ Main.java
@@ -1,25 +1,25 @@
 import ui.DrawPanel;
 import ui.Window;
-import hashlife.*;
+import life.hashlife.*;
 import util.RLE;
 
 public class Main {
 	
-	final static BooleanCell f = BooleanCell.False;
-	final static BooleanCell t = BooleanCell.True;
+	final static BooleanCell f = BooleanCell.off;
+	final static BooleanCell t = BooleanCell.on;
 	
 	static public void main(String[] args) {
-		Window w = new Window();
-		w.setVisible(true);
+		//Window w = new Window();
+		//w.setVisible(true);
 		
 		testHashlife();
-		testDrawer(w.getDrawPanel());
+		//testDrawer(w.getDrawPanel());
 		//testRLE();
 	}
 	
 	static void testRLE() {
 		
-		boolean[][] t = RLE.read("media/glider.rle");
+		int[][] t = RLE.read("media/glider.rle");
 		
 		System.out.println(MacroCell.niceStringFromTab(t));
 		
@@ -30,18 +30,18 @@ public class Main {
 		MacroCell b = get(f, f, t, f);
 		MacroCell c = get(t, t, f, f);
 		MacroCell d = get(t, f, f, f);
-		MacroCell planeur = get(a,b,c,d);
+		MacroCell planeur = Memoization.get(a, b, c, d);
 		
-		MacroCell r = planeur.evolve(1);
+		HashLifeState s = new HashLifeState(planeur);
 		
-		System.out.println(a.off);
-		System.out.println(planeur.simplify().niceString());
-		System.out.println(r.niceString());
-		System.out.println(r.evolve(4).niceString());
+		for(int i=0; i<10; i++) {
+			s.evolve(1);
+			System.out.println(s.state.niceString());
+		}
 	}
 	
 	static void testDrawer(DrawPanel drawer){
-		MacroCell a = get(f, t, f, f);
+		/*MacroCell a = get(f, t, f, f);
 		MacroCell b = get(f, f, t, f);
 		MacroCell c = get(t, t, f, f);
 		MacroCell d = get(t, f, f, f);
@@ -55,15 +55,15 @@ public class Main {
 			for(int i=0; i<1000000000; i++){
 				
 			}
-		}
+		}*/
 	}
 	
 	static MacroCell empty(int n) {
-		return MacroCell.empty(n);
+		return Memoization.empty(n);
 	}
 	
 	static MacroCell get(MacroCell m0, MacroCell m1, MacroCell m2, MacroCell m3) {
-		return MacroCell.get(m0, m1, m2, m3);
+		return Memoization.get(m0, m1, m2, m3);
 	}
 
 }
